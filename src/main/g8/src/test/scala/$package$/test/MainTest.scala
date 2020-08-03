c76e2949eceaf495c0d0fac560c285dbe62e7cb4
Andrew Rembrandt <me@andrewrembrandt.ch>
2020-08-03 18:27:19 +0200

    Updated for new form of ScalaTest

--- src/main/g8/src/test/scala/$package$/test/MainTest.scala
+++ src/main/g8/src/test/scala/$package$/test/MainTest.scala
@@ -1,6 +1,7 @@
 package $package$.test
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.flatspace.AnyFlatSpec
+import org.scalatest.matchers.should._
 
 class MainTest extends FlatSpec with Matchers {
 }
